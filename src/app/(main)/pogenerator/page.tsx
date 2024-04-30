c2d4cd3f4aefeb7f66c38ea5fcff41453879b3cc
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-29 22:06:32 -0300

    Create base pages

---
+++ src/app/(main)/pogenerator/page.tsx
@@ -0,0 +1,7 @@
+export default function POGeneratorPage() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>POGenerator Page</h1>
+      </main>
+    );
+  }
