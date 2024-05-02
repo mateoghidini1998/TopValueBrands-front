b7d8d06f7de420938036eccf14da20df3e6ee0d9
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-02 16:41:35 -0300

    Modify menu links styling

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link"
 import { LinkType } from "@/types/link.types"
 import { useState } from "react"
 import { usePathname } from "next/navigation"
@@ -16,14 +17,13 @@ const Menu = ({links}: MenuProps) => {
         <ul className="flex flex-col items-start w-full">
             {links.map((link, index) => (
                 <li key={`${index}`} className="mb-6 w-full flex items-center justify-start h-[40px]">
-                    <div className={`w-full h-[40px] flex items-center py-4 px-6 font-semibold ${pathname === link.href ? 'bg-[#438ef330] border-l-[6px] rounded-[2px] border-l-[#438EF3]' : ''}`}>
-                        <svg className="text-[22px] h-[26px] w-[26px] text-white mr-2.5">{link.icon}</svg>
-                        <a className="text-xs" href={link.href}>{link.title}</a>
-                    </div>
+                    <Link href={link.href} className={`box-border w-full h-[40px] flex items-center px-6 font-semibold transition-colors duration-300 ease-in-out ${pathname === link.href ? 'bg-[#438ef330] border-l-[6px] rounded-[2px] border-l-[#438EF3]' : ''}`}>
+                        <svg className="text-[22px] h-[26px] w-[26px] mr-2.5 text-[#438EF3]">{link.icon}</svg>
+                        <span className="text-xs">{link.title}</span>
+                    </Link>
                 </li>
             ))}
         </ul>
-
     )
 }
 
