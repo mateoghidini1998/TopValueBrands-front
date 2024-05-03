91a37bf33fc571aeee7c9f2bb5ccd4afc199883b
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-03 00:37:29 -0300

    Create Icons Components

---
+++ src/components/svgs/DotsSVG.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function DotsSVG() {
+  return (
+    <svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12.5002" cy="13.344" r="1.04167" transform="rotate(-90 12.5002 13.344)" stroke="white" stroke-width="2.08333" stroke-linecap="round"/>
+    <circle cx="12.5002" cy="19.594" r="1.04167" transform="rotate(-90 12.5002 19.594)" stroke="white" stroke-width="2.08333" stroke-linecap="round"/>
+    <circle cx="12.5002" cy="7.09401" r="1.04167" transform="rotate(-90 12.5002 7.09401)" stroke="white" stroke-width="2.08333" stroke-linecap="round"/>
+    </svg>
+
+  )
+}
