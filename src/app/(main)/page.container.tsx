d0ca253ec68fbdf7a521c43948ed26af86788424
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-04 19:58:32 -0300

    create index page container

---
+++ src/app/(main)/page.container.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { FC, PropsWithChildren, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import useAuthContext from "@/contexts/authContext";
+
+const Loader = () => <div>Cargando...</div>;
+
+const IndexPageContainer: FC<PropsWithChildren> = ({children}: PropsWithChildren) => {
+    const router = useRouter();
+    const { authToken, user } = useAuthContext();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (!authToken) {
+            router.push('/login');
+        } else if(user && user.role !== 'admin') {
+            router.push('/pogenerator');
+            router.refresh();
+        } else {
+            router.push('/');
+        }
+        setIsLoading(false);
+    }, [authToken, user, router]);
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    return authToken ? children : null;
+}
+
+export default IndexPageContainer;
