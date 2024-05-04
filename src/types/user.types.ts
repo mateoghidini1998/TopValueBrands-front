a8e63fb008430a2a0fd31112b30a697a4e9216d1
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-04 19:56:43 -0300

    add role to User Type

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,5 +1,12 @@
+enum UserRole  {
+    ADMIN = "admin",
+    USER = "user",
+    EMPLOYEE = "employee"
+}
+
 export type UserType = {
     firstName: string,
     lastName: string,
     email: string,
+    role: UserRole
 }
