a9526d78340fd9dcea415e8c245560de37716e66
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-04 20:00:09 -0300

    Add pagination params to request

--- src/services/inventory/inventory.ts
+++ src/services/inventory/inventory.ts
@@ -2,10 +2,10 @@ import { HttpAPI } from "../common/http.service";
 import { getAuthToken } from "@/utils/getAuthToken";
 
 export class InventoryService {
-    static async getProducts() {
+    static async getProducts(page: number, limit: number) {
         try {
             const token = getAuthToken();
-            const response = await HttpAPI.get('http://localhost:3000/api/v1/products', {
+            const response = await HttpAPI.get(`http://localhost:3000/api/v1/products?page=${page}&limit=${limit}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
