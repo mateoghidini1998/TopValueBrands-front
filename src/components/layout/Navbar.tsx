4a9d67e8bb8392082ce87ed822eeaf3778f57bfa
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-04 19:59:19 -0300

    Dinamically render links

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,15 +5,26 @@ import Users from "../svgs/Users"
 import Shop from "../svgs/Shop"
 import Search from "../svgs/Search"
 import Package from "../svgs/Package"
+import useAuthContext from "@/contexts/authContext"
+import { useEffect } from "react"
 
 const LINKS = [
     { title: "Inventory Management", href: '/', icon: <Search/> },
     { title: "PO Generator", href: '/pogenerator', icon: <Package/> },
     { title: "Warehouse", href: '/warehouse', icon: <Shop/> },
-    { title: "Users ", href: '/users', icon: <Users/> },
+    { title: "Users", href: '/users', icon: <Users/> },
 ]
 
 const Navbar = () => {
+    const { user } = useAuthContext()
+
+    const filteredLinks = LINKS.filter(link => {
+        if (!user || (user.role !== 'admin' && (link.title === "Users" || link.title === "Inventory Management"))) {
+            return false;
+        }
+        return true;
+    });
+
     return (
         <nav className="flex flex-col min-w-[275px] border-r-[1px] border-r-[#262935] text-white box-border">
             <h1 className="text-lg font-bold px-12 pb-[70px] pt-9 text-center">Top Value Brands</h1>
@@ -21,11 +32,11 @@ const Navbar = () => {
 
             {/* Nav Items */} 
             <div className="flex flex-col items-center w-full">
-                <Menu links={LINKS} />
+                <Menu links={filteredLinks} />
             </div>
-
         </nav>
     )
 }
+
 
 export default Navbar;
