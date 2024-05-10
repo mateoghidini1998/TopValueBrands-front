617e50300a3bf68046cb55bec35fb705368094e1
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-10 12:58:55 -0300

    Remove trycatch

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,27 +1,23 @@
-import { cookies } from "next/headers";
 import { HttpAPI } from "../common/http.service";
 export class AuthService {
     static async login(email: string, password: string, onUserLoaded: (user: any) => void) {
-        try {
-            const response = await HttpAPI.post('http://localhost:3000/api/v1/auth/login', { email, password });
+            const response = await HttpAPI.post('http://localhost:5000/api/v1/auth/login', { email, password });
             localStorage.setItem('access-token', response.token);
             
-            const userResponse = await HttpAPI.get('http://localhost:3000/api/v1/auth/me', {
+            const userResponse = await HttpAPI.get('http://localhost:5000/api/v1/auth/me', {
                 headers: {
                     'Authorization': `Bearer ${response.token}`
                 }
             });
             onUserLoaded(userResponse.data);
             document.cookie = "authenticated=true; path=/";
+            console.log("Respondiendo con error")
             return response;
-        } catch (error) {
-            throw new Error('Error while login in')
-        }
     }
 
     static async getUserProfile(token: string) {
         try {
-            const response = await HttpAPI.get('http://localhost:3000/api/v1/auth/me', {
+            const response = await HttpAPI.get('http://localhost:5000/api/v1/auth/me', {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
