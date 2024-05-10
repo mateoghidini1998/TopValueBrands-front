e30f93afec4e98e00476c176e63348b8d8888fb8
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-10 12:59:23 -0300

    Add deactivate product method

--- src/services/inventory/inventory.ts
+++ src/services/inventory/inventory.ts
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { HttpAPI } from "../common/http.service";
 import { getAuthToken } from "@/utils/getAuthToken";
 
@@ -5,7 +6,7 @@ export class InventoryService {
     static async getProducts(page: number, limit: number) {
         try {
             const token = getAuthToken();
-            const response = await HttpAPI.get(`http://localhost:3000/api/v1/products?page=${page}&limit=${limit}`, {
+            const response = await HttpAPI.get(`http://localhost:5000/api/v1/products?page=${page}&limit=${limit}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
@@ -15,4 +16,21 @@ export class InventoryService {
             throw new Error('Error fetching data');
         }
     }
+
+    static async deactivateProduct(seller_sku: string) {
+        try {
+            const token = getAuthToken();
+            if (token) {
+                const response = await HttpAPI.patch(`http://localhost:5000/api/v1/products/disable`, {seller_sku: seller_sku}, token);
+                return response;
+            } else {
+                throw new Error('Token not found');
+            }
+        } catch (error) {
+            throw new Error('Error fetching data');
+        }
+    }
+
+    
+
 }
