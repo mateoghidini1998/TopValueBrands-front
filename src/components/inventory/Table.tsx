369af77b96afd31b79bd788ab026bb9577759a0c
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-10 12:58:00 -0300

    Add pagination logic

--- src/components/inventory/Table.tsx
+++ src/components/inventory/Table.tsx
@@ -1,16 +1,30 @@
 "use client"
 
-import { FC, useState } from "react";
-import UserMenu from "../layout/UserMenu";
-import SearchInput from "./SearchInput";
+import { FC, useEffect, useState } from "react";
 import TableRow from "./TableRow";
 import Pagination from "./Pagination";
+import { InventoryService } from "@/services/inventory/inventory";
 
 const Table:FC = () => {
-
+    const [products, setProducts] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
     const limit = 10;
 
+    useEffect(() => {
+        getProducts(currentPage, limit);
+    }, [currentPage, limit]);
+
+
+    const getProducts = async (page: number, limit: number) => {
+      try {
+          const response = await InventoryService.getProducts(page, limit);
+          setProducts(response.data);
+          setTotalPages(response.pages)
+      } catch (error) {
+          console.error(error);
+      }
+    };
     const handleNextPage = () => {
         setCurrentPage(prevPage => prevPage + 1);
     };
@@ -30,17 +44,18 @@ const Table:FC = () => {
             <th className="w-[10%] text-xs font-medium text-center">Supplier name</th>
             <th className="w-[10%] text-xs font-medium text-center">Supplier item No</th>
             <th className="w-[5%] text-xs font-medium text-center">Pack type</th>
-            <th className="w-[10%] text-xs font-medium text-center">FBA Stock</th>
+            <th className="w-[5%] text-xs font-medium text-center">FBA Stock</th>
             <th className="w-[5%] text-xs font-medium text-center">Reserved quantity</th>
             <th className="w-[5%] text-xs font-medium text-center">Inbound to FBA</th>
           </tr>
-          <TableRow currentPage={currentPage} limit={limit} />
+          <TableRow products={products}/>
         </table>
         <Pagination
           currentPage={currentPage}
           setCurrentPage={setCurrentPage}
           handleNextPage={handleNextPage}
           handlePreviousPage={handlePreviousPage}
+          totalPages={totalPages}
         />
     </>
     )
