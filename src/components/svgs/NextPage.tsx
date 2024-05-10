e18c7d3f6afb4356b06ed7c895b90df4af77cad2
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-10 17:13:43 -0300

    Fix pagination styling

--- src/components/svgs/NextPage.tsx
+++ src/components/svgs/NextPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function NextPage() {
+type NextPageProps = {
+  disabled: boolean;
+}
+
+export default function NextPage({ disabled }: NextPageProps) {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="Expand_down_light">
-    <path id="Vector 9" d="M9 6L15 12L9 18" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path id="Vector 9" d="M9 6L15 12L9 18" stroke={disabled ? "#393E4F" : "white"} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     </g>
     </svg>
   )
