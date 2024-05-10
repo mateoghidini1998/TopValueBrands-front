2535ce7848beb6d18e2e62a82f2df44552f8f1a4
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-10 15:18:29 -0300

    Edit products on inventory table function

--- src/components/inventory/TableRow.tsx
+++ src/components/inventory/TableRow.tsx
@@ -2,7 +2,7 @@
 import { ProductType } from "@/types/product.types";
 import DotsSVG from "../svgs/DotsSVG";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RowActions from "./Actions";
 import { InventoryService } from "@/services/inventory/inventory";
 
@@ -14,22 +14,22 @@ const TableRow = ({ products }: TableRowProps) => {
   const [isActionsOpen, setIsActionsOpen] = useState< string | null >(null);
   const [editingRow, setEditingRow] = useState<{ [key: string]: boolean }>({});
   const [currentProduct, setCurrentProduct] = useState<{ seller_sku: string } | null>(null);
-  const [ editData, setEditData ] = useState({
-    product_cost: '',
-    supplier_name: '',
-    supplier_item_number: '',
-    pack_type: ''
-  })
-  
-  const { product_cost, supplier_name, supplier_item_number, pack_type } = editData;
+  const [ editData, setEditData ] = useState({})
 
-  const handleToggleActions = (seller_sku: string) => {
+  // const { product_cost, supplier_name, supplier_item_number, pack_type } = editData;
+
+
+
+  const handleToggleActions = (seller_sku: string, product: ProductType) => {
     if (isActionsOpen === seller_sku) {
       setIsActionsOpen(null);
     } else {
       setIsActionsOpen(seller_sku);
 
     }
+
+    setEditData(product)
+
   };
   
 
@@ -39,7 +39,13 @@ const TableRow = ({ products }: TableRowProps) => {
   };
 
   const handleSave = async () => {
+    try {
+      const response = await InventoryService.updateProduct({...editData, seller_sku: currentProduct?.seller_sku?? '' })
+    } catch (error) {
+      console.error('Error al actualizar el producto: ', error);
+    }
     setEditingRow({});
+    setIsActionsOpen(null);
   };
 
   const handleDelete = async(seller_sku: string) => {
@@ -52,7 +58,7 @@ const TableRow = ({ products }: TableRowProps) => {
 
   const onChange = e => setEditData({
     ...editData, [e.target.name]:e.target.value
-});
+  });
   
 
   
@@ -90,28 +96,28 @@ const TableRow = ({ products }: TableRowProps) => {
             <td className="w-[10%] text-xs font-medium text-center">{product.seller_sku}</td>
             <td className="w-[5%] text-xs font-medium text-center flex justify-center">
               {editingRow[product.seller_sku] ? (
-                <input name="product_cost" type="text"className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={product.product_cost} onChange={e => onChange(e)} />
+                <input name="product_cost" type="text"className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={editData.product_cost} onChange={e => onChange(e)} />
               ) : (
                 product.product_cost
               )}
             </td>
             <td className="w-[10%] text-xs font-medium text-center flex justify-center">
               {editingRow[product.seller_sku] ? (
-                <input name="supplier_name" type="text"className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={product.supplier_name} onChange={e => onChange(e)} />
+                <input name="supplier_name" type="text"className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={editData.supplier_name} onChange={e => onChange(e)} />
               ) : (
                 product.supplier_name
               )}
             </td>
             <td className="w-[10%] text-xs font-medium text-center flex justify-center">
               {editingRow[product.seller_sku] ? (
-                <input name="supplier_item_number" type="text"className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={product.supplier_item_number} onChange={e => onChange(e)} />
+                <input name="supplier_item_number" type="text"className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={editData.supplier_item_number} onChange={e => onChange(e)} />
               ) : (
                 product.supplier_item_number
               )}
             </td>
             <td className="w-[5%] text-xs font-medium text-center flex justify-center">
               {editingRow[product.seller_sku] ? (
-                <input name="pack_type" type="text" className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={product.pack_type} onChange={e => onChange(e)} />
+                <input name="pack_type" type="text" className="w-2/3 p-1 rounded-lg text-center text-[#55597D] bg-[#262935] border-[1px] border-solid border-[#393E4F]" value={editData.pack_type} onChange={e => onChange(e)} />
               ) : (
                 product.pack_type
               )}
@@ -119,10 +125,20 @@ const TableRow = ({ products }: TableRowProps) => {
             <td className="w-[5%] text-xs font-medium text-center">{product.FBA_available_inventory}</td>
             <td className="w-[5%] text-xs font-medium text-center">{product.reserved_quantity}</td>
             <td className="w-[5%] text-xs font-medium text-center">{product.Inbound_to_FBA}</td>
-            <td className="w-[10%] text-xs font-medium text-center relative">
-              <button onClick={() => handleToggleActions(product.seller_sku)}>
+              <td className="w-[10%] text-xs font-medium text-center relative">
+                
+                {
+                  !editingRow[product.seller_sku]
+                  ?
+              <button onClick={() => handleToggleActions(product.seller_sku, product)}>
                   <DotsSVG/>
               </button>
+                    :
+                    <button onClick={() => handleSave()}>
+                      Save
+                    </button>
+                }
+
               {isActionsOpen === product.seller_sku && <RowActions onEdit={() => handleEdit(product.seller_sku)} onDelete={() => handleDelete(product.seller_sku)} />}
           </td>
           </tr>
--- src/services/inventory/inventory.ts
+++ src/services/inventory/inventory.ts
@@ -1,36 +1,63 @@
 import { headers } from "next/headers";
 import { HttpAPI } from "../common/http.service";
 import { getAuthToken } from "@/utils/getAuthToken";
+import { ProductType } from "@/types/product.types";
 
 export class InventoryService {
-    static async getProducts(page: number, limit: number) {
-        try {
-            const token = getAuthToken();
-            const response = await HttpAPI.get(`http://localhost:5000/api/v1/products?page=${page}&limit=${limit}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            return response;
-        } catch (error) {
-            throw new Error('Error fetching data');
+  static async getProducts(page: number, limit: number) {
+    try {
+      const token = getAuthToken();
+      const response = await HttpAPI.get(
+        `http://localhost:5000/api/v1/products?page=${page}&limit=${limit}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
+      return response;
+    } catch (error) {
+      throw new Error("Error fetching data");
     }
+  }
 
-    static async deactivateProduct(seller_sku: string) {
-        try {
-            const token = getAuthToken();
-            if (token) {
-                const response = await HttpAPI.patch(`http://localhost:5000/api/v1/products/disable`, {seller_sku: seller_sku}, token);
-                return response;
-            } else {
-                throw new Error('Token not found');
-            }
-        } catch (error) {
-            throw new Error('Error fetching data');
+  static async deactivateProduct(seller_sku: string) {
+    try {
+      const token = getAuthToken();
+      if (token) {
+        const response = await HttpAPI.patch(
+          `http://localhost:5000/api/v1/products/disable`,
+          { seller_sku: seller_sku },
+          token
+        );
+        return response;
+      } else {
+        throw new Error("Token not found");
+      }
+    } catch (error) {
+      throw new Error("Error fetching data");
+    }
+  }
+
+  static async updateProduct(data: any) {
+    try {
+      const token = getAuthToken();
+      if (token) {
+        if (!data.seller_sku) {
+          throw new Error("seller_sku is required");
         }
+        const response = await HttpAPI.patch(
+          `http://localhost:5000/api/v1/products/addExtraInfoToProduct`,
+          data,
+          token
+        );
+        return response;
+      } else {
+        throw new Error("Token not found");
+      }
+    } catch (error) {
+      throw new Error("Error updating product");
     }
-
-    
+  }
 
 }
