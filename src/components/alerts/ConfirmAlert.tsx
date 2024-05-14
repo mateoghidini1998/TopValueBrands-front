6e028f4ce2c05a2e5c56ca1afb43c928e5fd08ba
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-14 14:16:53 -0300

    finish tooltip and fix alerts color and position

--- src/components/alerts/ConfirmAlert.tsx
+++ src/components/alerts/ConfirmAlert.tsx
@@ -25,14 +25,14 @@ const ConfirmAlert = forwardRef(function ConfirmAlert({ message, onConfirm, onCa
 
   return (
     <>
-        <dialog open ref={dialog} className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[320px] h-[226px] bg-[#262935] z-[2000] flex flex-col items-center justify-center gap-6 px-[2rem] rounded-md">
+        <dialog open ref={dialog} className="absolute top-[50%] left-0 translate-y-[-50%] w-[320px] h-[226px] bg-[#262935] z-[2000] flex flex-col items-center justify-center gap-6 px-[2rem] rounded-md">
           <span onClick={onClose} className="absolute top-2 right-2 cursor-pointer">
             <CloseButton/>
           </span>
-          <p className="text-center">{message}</p>
+          <p className="text-center text-white">{message}</p>
           <div className="flex w-[90%] items-center justify-center gap-2 ">
-            <button onClick={onConfirm} className="w-[100px] h-[40px] bg-[#438EF3] rounded-md ">{confirmText}</button>
-            <button onClick={onCancel} className="w-[100px] h-[40px] bg-[#393E4F] rounded-md ">{cancelText}</button>  
+            <button onClick={onConfirm} className="text-white w-[100px] h-[40px] bg-[#438EF3] rounded-md ">{confirmText}</button>
+            <button onClick={onCancel} className="text-white w-[100px] h-[40px] bg-[#393E4F] rounded-md ">{cancelText}</button>  
           </div>
         </dialog>
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-filter backdrop-blur-md flex justify-center items-center z-[1100]"></div>
--- src/components/inventory/Tooltip.tsx
+++ src/components/inventory/Tooltip.tsx
@@ -1,11 +1,26 @@
+"use client";
+import { useState } from "react";
+
 type TooltipProps = {
   product_name: string;
 };
 
 export const Tooltip = ({ product_name }: TooltipProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="absolute z-50  h-[20px] p-0 hover:p-6 rounded-sm bg-transparent text-transparent top-10 left-5 w-11/12 hover:h-auto hover:bg-[#393E4F] hover:text-white">
-      <p>{product_name}</p>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="absolute z-50  h-[20px] p-0 hover:p-6 rounded-sm bg-transparent text-transparent top-[50px] left-5 w-11/12 hover:h-auto hover:bg-[#262935] hover:text-white">
+        {isHovered && (
+          <>
+            <div className="absolute h-0 w-0 border-[10px] border-transparent border-t-[#262935] left-1/2 transform -translate-x-1/2 bg-transparent top-[-16px] rotate-180"></div>
+            <p>{product_name}</p>
+          </>
+        )}
+      </div>
     </div>
   );
 };
