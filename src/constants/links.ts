36ddd85349bd17a877baa23d274ba7588a6437a2
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 11:03:51 -0300

    rename links

--- src/constants/links.ts
+++
@@ -1,6 +0,0 @@
-export const LINKS = [
-    { title: "Inventory Management", href: '/'},
-    { title: "PO Generator", href: '/pogenerator'},
-    { title: "Warehouse", href: '/warehouse'},
-    { title: "Users", href: '/users'},
-]
