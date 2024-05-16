cb1a4f1c92f250ad030859004d405a63bb2b8de7
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 11:24:55 -0300

    Add confirmPassword validation

--- src/schemes/register.scheme.ts
+++ src/schemes/register.scheme.ts
@@ -5,6 +5,7 @@ const RegisterScheme = yup.object().shape({
   lastName: yup.string().required(),
   email: yup.string().email().required(),
   password: yup.string().min(8).required(),
+  confirmPassword: yup.string().min(8).required(),
   role: yup.string().required(),
 });
 export default RegisterScheme;
