721e35b119be9951a4f457f81fb105f16b7c4b30
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-15 22:59:09 -0300

    Fix svg properties errors

--- src/components/svgs/PrevPage.tsx
+++ src/components/svgs/PrevPage.tsx
@@ -7,7 +7,7 @@ type PrevPageProps = {
 export default function PrevPage({ disabled }: PrevPageProps) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-    <path d="M15 18L9 12L15 6" stroke={disabled ? "#393E4F" : "white"} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M15 18L9 12L15 6" stroke={disabled ? "#393E4F" : "white"} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
 
   )
--- src/components/svgs/Search.tsx
+++ src/components/svgs/Search.tsx
@@ -1,12 +1,12 @@
 export default function Search() {
   return (
     <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8.25 6.41664L11.9167 6.41664" stroke="#438EF3" stroke-width="1.5" stroke-linecap="round"/>
-    <path d="M8.25 13.75L11 13.75" stroke="#438EF3" stroke-width="1.5" stroke-linecap="round"/>
-    <path d="M8.25 10.0834L13.75 10.0834" stroke="#438EF3" stroke-width="1.5" stroke-linecap="round"/>
-    <path d="M17.4168 10.0833V8.25C17.4168 5.65728 17.4168 4.36091 16.6113 3.55546C15.8058 2.75 14.5095 2.75 11.9168 2.75H10.0834C7.4907 2.75 6.19434 2.75 5.38888 3.55546C4.58342 4.36091 4.58342 5.65727 4.58342 8.25V13.75C4.58342 16.3427 4.58342 17.6391 5.38888 18.4445C6.19434 19.25 7.4907 19.25 10.0834 19.25H11.0001" stroke="#438EF3" stroke-width="1.5" stroke-linecap="round"/>
-    <circle cx="16.0417" cy="16.0417" r="2.29167" stroke="#438EF3" stroke-width="1.5" stroke-linecap="round"/>
-    <path d="M19.25 19.25L17.875 17.875" stroke="#438EF3" stroke-width="1.5" stroke-linecap="round"/>
+    <path d="M8.25 6.41664L11.9167 6.41664" stroke="#438EF3" strokeWidth="1.5" strokeLinecap="round"/>
+    <path d="M8.25 13.75L11 13.75" stroke="#438EF3" strokeWidth="1.5" strokeLinecap="round"/>
+    <path d="M8.25 10.0834L13.75 10.0834" stroke="#438EF3" strokeWidth="1.5" strokeLinecap="round"/>
+    <path d="M17.4168 10.0833V8.25C17.4168 5.65728 17.4168 4.36091 16.6113 3.55546C15.8058 2.75 14.5095 2.75 11.9168 2.75H10.0834C7.4907 2.75 6.19434 2.75 5.38888 3.55546C4.58342 4.36091 4.58342 5.65727 4.58342 8.25V13.75C4.58342 16.3427 4.58342 17.6391 5.38888 18.4445C6.19434 19.25 7.4907 19.25 10.0834 19.25H11.0001" stroke="#438EF3" strokeWidth="1.5" strokeLinecap="round"/>
+    <circle cx="16.0417" cy="16.0417" r="2.29167" stroke="#438EF3" strokeWidth="1.5" strokeLinecap="round"/>
+    <path d="M19.25 19.25L17.875 17.875" stroke="#438EF3" strokeWidth="1.5" strokeLinecap="round"/>
     </svg>
 
   )
