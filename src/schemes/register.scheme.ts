172670f1a6eb85b1af9a4aa071b766a8017ea957
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-15 21:45:17 -0300

    create register form scheme

---
+++ src/schemes/register.scheme.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup'
+
+const RegisterScheme = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required(),
+  role: yup.string().required(),
+});
+export default RegisterScheme;
