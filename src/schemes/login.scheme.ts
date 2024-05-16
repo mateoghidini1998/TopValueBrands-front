bbc0924d4245037e2c147a29996f1b3539e0e9c2
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 11:24:43 -0300

    add email validation

--- src/schemes/login.scheme.ts
+++ src/schemes/login.scheme.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 
 const LoginScheme = yup.object({
-    email: yup.string().required(),
+    email: yup.string().email().required(),
     password: yup.string().required()
 }).required();
 
