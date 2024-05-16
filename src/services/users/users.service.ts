90eaec982000d7ae6903538ad2bd36627bee9db0
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-15 23:05:05 -0300

    Modify the addUser method to return the response to catch errors

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -3,37 +3,30 @@ import { HttpAPI } from "../common/http.service";
 import { UserType } from "@/types/user.types";
 
 export class UsersService {
-    static async getUsers() {
-        try {
-            const token = getAuthToken();
-            const response = await HttpAPI.get(
-                `http://localhost:5000/api/v1/users`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                }
-            )
-            return response;
-        } catch (error) {
-            throw new Error("Error fetching data")
-        }
+  static async getUsers() {
+    try {
+      const token = getAuthToken();
+      const response = await HttpAPI.get(`http://localhost:5000/api/v1/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response;
+    } catch (error) {
+      throw new Error("Error fetching data");
     }
+  }
 
-    static async addUser(data: UserType) {
-        try {
-            const token = getAuthToken();
+  static async addUser(data: UserType) {
+    const token = getAuthToken();
 
-            if(!token) throw new Error("Token not found")
+    if (!token) throw new Error("Token not found");
 
-            const response = await HttpAPI.post(
-                `http://localhost:5000/api/v1/auth/register`,
-                data,
-                token
-            )
-            return response;
-
-        } catch (error) {
-            throw new Error("Error creating user");
-        }
-    }
+    const response = await HttpAPI.post(
+      `http://localhost:5000/api/v1/auth/register`,
+      data,
+      token
+    );
+    return response;
+  }
 }
