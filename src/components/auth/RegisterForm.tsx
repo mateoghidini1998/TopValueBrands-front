11a02ccda4f38e4d0783666d3d8385bfff297e76
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-15 22:58:05 -0300

    Add dropdown for users role on register form

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -6,42 +6,61 @@ import RegisterScheme from "@/schemes/register.scheme";
 import SubmitButton from "../form/SubmitButton";
 import CloseButton from "../svgs/CloseButton";
 import { UserRole } from "@/types/user.types";
+import { RegisterErrorCard } from "./RegisterErrorCard";
 
 type FormData = {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    role: UserRole;
-  } 
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  role: UserRole;
+};
 
-const RegisterForm = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
-    const { addUser } = useUsersContext();
-  
-  
-    const methods = useForm<FormData>({
-      resolver: yupResolver(RegisterScheme) as unknown as Resolver<FormData>,
-    });
-  
-    const { handleSubmit } = methods;
-    
-    const onSubmit = (data: FormData) => {
-      try {
-        addUser(data);
-        console.log(data)
-      } catch (error) {
-        
-      }
+const RegisterForm = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const { addUser, registerError } = useUsersContext();
+
+  const methods = useForm<FormData>({
+    resolver: yupResolver(RegisterScheme) as unknown as Resolver<FormData>,
+  });
+
+  const { handleSubmit } = methods;
+
+  const onSubmit = (data: FormData) => {
+    try {
+      addUser(data).then((response) => {
+        if (response.success) {
+          onClose();
+        }
+      });
+    } catch (error) {
+      console.error(error);
     }
-    
-    if(!isOpen) return null;
-  
-    return (
-      <>
-      <dialog open className="min-w-[400px] max-w-[700px] shadow-[0_0_50px_0_rgba(0,0,0,0.2)] rounded-2xl py-12 px-14 bg-[#262935] fixed z-[2000] inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <dialog
+        open
+        className="min-w-[400px] max-w-[700px] shadow-[0_0_50px_0_rgba(0,0,0,0.2)] rounded-2xl py-12 px-14 bg-[#262935] fixed z-[2000] inset-0 overflow-y-auto"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        {registerError && <RegisterErrorCard errorMessage={registerError} />}
         <div className="w-full flex items-center justify-between mb-8">
-          <h3 className="text-xl text-white font-medium">Create new User</h3>    
-          <button onClick={onClose}><CloseButton/></button>
+          <h3 className="text-xl text-white font-medium">Create new User</h3>
+          <button onClick={onClose}>
+            <CloseButton />
+          </button>
         </div>
         <FormProvider {...methods}>
           <form action="" onSubmit={handleSubmit(onSubmit)}>
@@ -55,18 +74,29 @@ const RegisterForm = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => voi
               <InputText label="Email" fieldName="email" type="text" />
             </div>
             <div className="flex flex-col mb-5">
-              <InputText label="Role" fieldName="role" type="text" />
+              <InputText label="Role" fieldName="role" type="text" isDropdown />
             </div>
             <div className="flex flex-col mb-5">
-              <InputText label="Password" fieldName="password" type="password" />
+              <InputText
+                label="Password"
+                fieldName="password"
+                type="password"
+              />
             </div>
-            <SubmitButton label="Create" onSubmit={onSubmit}/>
+            <div className="flex flex-col mb-5">
+              <InputText
+                label="Confirm Password"
+                fieldName="confirmPassword"
+                type="password"
+              />
+            </div>
+            <SubmitButton label="Create" onSubmit={onSubmit} />
           </form>
         </FormProvider>
       </dialog>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-filter backdrop-blur-md flex justify-center items-center z-[1100]"></div>
-      </>
-    )
-}
+    </>
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
