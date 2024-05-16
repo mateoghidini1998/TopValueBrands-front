1d868f898e5ac37f0d0c5ce75fc36a4fc367b4d6
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-15 21:44:55 -0300

    Create users table component

--- src/app/(main)/users/page.tsx
+++ src/app/(main)/users/page.tsx
@@ -1,7 +1,13 @@
+import Table from "@/components/users/Table";
+import { UsersProvider } from "@/contexts/users.context";
+
 export default function UsersPage() {
     return (
-      <main className="flex w-full min-h-screen flex-col items-center">
-        <h1>POGenerator Page</h1>
-      </main>
+      <UsersProvider>
+        <main className="flex w-full p-14 min-h-screen flex-col items-center">
+          <h1>POGenerator Page</h1>
+          <Table/>
+        </main>
+      </UsersProvider>
     );
   }
