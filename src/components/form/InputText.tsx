fdf115178916b56e47d7f45b71c41d813ce1f99e
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-15 23:02:02 -0300

    Add the dropdown field condition

--- src/components/form/InputText.tsx
+++ src/components/form/InputText.tsx
@@ -1,27 +1,50 @@
 import { useFormContext } from "react-hook-form";
 
 type InputTextProps = {
-    styles?: string;
-    fieldName: string;
-    label: string;
-    placeholder?: string;
-    type: 'text' | 'password'
-}
+  styles?: string;
+  fieldName: string;
+  isDropdown?: boolean;
+  label: string;
+  placeholder?: string;
+  type: "text" | "password";
+};
 
-const InputText = ({ label, styles, fieldName, type,  placeholder }: InputTextProps) => {
-    const { register, formState: { errors } } = useFormContext();
-    return (
-        <>
-            <label className="text-[#808191] text-sm mb-2" htmlFor="">{label}:</label>
-            <input
-            {...register(fieldName)} 
-            placeholder={placeholder} 
-            type={type}
-            className="p-2.5 bg-[#1F2128] shrink rounded-md text-white focus:outline-none"
-            />
-            {errors && errors[fieldName] && <div className="text-xs text-red-600">This field is required</div>}
-        </>
-    )
-}
+const InputText = ({
+  label,
+  styles,
+  fieldName,
+  type,
+  placeholder,
+  isDropdown,
+}: InputTextProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <>
+      <label className="text-[#808191] text-sm mb-2" htmlFor="">
+        {label}:
+      </label>
+
+      {isDropdown ? (
+        <select {...register(fieldName)} className="p-2.5 bg-[#1F2128] shrink rounded-md text-white focus:outline-none">
+          <option className="" value="admin">Admin</option>
+          <option className="" value="user">User</option>
+        </select>
+      ) : (
+        <input
+          {...register(fieldName)}
+          placeholder={placeholder}
+          type={type}
+          className="p-2.5 bg-[#1F2128] shrink rounded-md text-white focus:outline-none"
+        />
+      )}
+      {errors && errors[fieldName] && (
+        <div className="text-xs text-red-600">This field is required</div>
+      )}
+    </>
+  );
+};
 
 export default InputText;
