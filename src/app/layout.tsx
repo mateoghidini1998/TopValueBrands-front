cf1f09eb6b10089564d2d4b447374a4fb01d5e44
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-31 20:29:39 -0300

    prevente light mode flash when login out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,17 @@ import "./globals.css";
 import { Suspense } from "react";
 import Loading from "./loading";
 
+const setInitialThemeScript = `
+  (function() {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  })();
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logo_tvb.png" />
+        <script dangerouslySetInnerHTML={{ __html: setInitialThemeScript }} />
         {/* Puedes agregar más meta tags aquí si es necesario */}
       </head>
       <ThemeProvider>
