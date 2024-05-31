c3751ac42ccb5d6abe2977c6ec7703696b9a3f3a
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-31 18:40:51 -0300

    add skeleton during loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
-import { AuthProvider } from '@/contexts/auth.context';
-import { ThemeProvider } from '@/contexts/theme.context';
-import './globals.css';
+import { AuthProvider } from "@/contexts/auth.context";
+import { ThemeProvider } from "@/contexts/theme.context";
+import "./globals.css";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo_tvb.png" />
+        {/* Puedes agregar más meta tags aquí si es necesario */}
+      </head>
       <ThemeProvider>
         <body className="flex bg-white dark:bg-dark">
-          <AuthProvider>
-            {children}
-          </AuthProvider>
+          <Suspense fallback={<Loading />}>
+            <AuthProvider>{children}</AuthProvider>
+          </Suspense>
         </body>
       </ThemeProvider>
-      </html>
+    </html>
   );
 }
