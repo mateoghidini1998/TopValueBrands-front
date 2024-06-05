e6f12de26a378f80808ac84a4715ab5fe1fa30ca
ramirosarasola <ramiro.sarasola@gmail.com>
2024-06-05 13:26:07 -0300

    fix the lightmode styles for the pagination on the inventory table

--- src/components/svgs/NextPage.tsx
+++ src/components/svgs/NextPage.tsx
@@ -1,3 +1,4 @@
+import useThemeContext from '@/contexts/theme.context';
 import React from 'react'
 
 type NextPageProps = {
@@ -5,10 +6,17 @@ type NextPageProps = {
 }
 
 export default function NextPage({ disabled }: NextPageProps) {
+
+  const { theme } = useThemeContext();
+
+  const color = theme === "dark" ? "white" : "black";
+  const disabledColor = theme === "dark" ? "#393E4F" : "#858EAD";
+
+
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="Expand_down_light">
-    <path id="Vector 9" d="M9 6L15 12L9 18" stroke={disabled ? "#393E4F" : "white"} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path id="Vector 9" d="M9 6L15 12L9 18" stroke={disabled ? disabledColor : color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </g>
     </svg>
   )
--- src/components/svgs/PrevPage.tsx
+++ src/components/svgs/PrevPage.tsx
@@ -1,13 +1,20 @@
+import useThemeContext from '@/contexts/theme.context';
 import React from 'react'
 
 type PrevPageProps = {
   disabled: boolean;
 }
 
+
 export default function PrevPage({ disabled }: PrevPageProps) {
+  const { theme } = useThemeContext();
+
+  const color = theme === "dark" ? "white" : "black";
+  const disabledColor = theme === "dark" ? "#393E4F" : "#858EAD";
+
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-    <path d="M15 18L9 12L15 6" stroke={disabled ? "#393E4F" : "blue"} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M15 18L9 12L15 6" stroke={disabled ? disabledColor : color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
 
   )
