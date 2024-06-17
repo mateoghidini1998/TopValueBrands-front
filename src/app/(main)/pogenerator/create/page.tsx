e5495f8a6a7c2b67d3deda0e12668292e7a60bea
ramirosarasola <ramiro.sarasola@gmail.com>
2024-06-17 15:02:55 -0300

    update the generator products table with the tracked products data

--- src/app/(main)/pogenerator/create/page.tsx
+++ src/app/(main)/pogenerator/create/page.tsx
@@ -1,52 +1,58 @@
-import { data } from "@/app/data";
+"use client";
 import DeleteIcon from "@/components/svgs/DeleteIcon";
+import { useTrackedProductContext } from "@/contexts/trackedProducts.context";
+import { TrackedProductType } from "@/types/trackedProducts.types";
 import IndexPageContainer from "../../page.container";
 import { InputOrderAction } from "../components/InputOrderAction";
 import { TableComponent } from "../components/TableComponent";
 import { Column } from "../interfaces/ITableComponent";
-import { TrackedProducts } from "../interfaces/ITrackedProducts";
-
-const products: TrackedProducts[] = data.products;
-const orderProducts: TrackedProducts[] = data.orderProducts;
 
 const trackedProductsCol: Column[] = [
-  { key: "product", name: "Product", width: "100px" },
-  { key: "supplier_name", name: "Supplier Name", width: "100px" },
-  { key: "asin", name: "ASIN", width: "100px" },
-  { key: "amazon_sku", name: "Amazon SKU", width: "100px" },
-  { key: "current_rank", name: "Current Rank", width: "100px" },
-  { key: "30_day_rank", name: "30 Day Rank", width: "100px" },
-  { key: "90_day_rank", name: "90 Day Rank", width: "100px" },
-  { key: "units_sold", name: "Units Sold", width: "100px" },
-  { key: "velocity", name: "Velocity", width: "100px" },
+  { key: "product_name", name: "Product", width: "300px" },
+  { key: "ASIN", name: "ASIN", width: "150px" },
+  { key: "seller_sku", name: "Amazon SKU", width: "150px" },
+  { key: "supplier_name", name: "Supplier Name", width: "150px" },
+  { key: "thirty_days_rank", name: "30 Day Rank", width: "150px" },
+  { key: "ninety_days_rank", name: "90 Day Rank", width: "150px" },
+  { key: "units_sold", name: "Units Sold", width: "150px" },
+  { key: "product_velocity", name: "Velocity", width: "150px" },
+  { key: "lowest_fba_price", name: "Lowest FBA Price ", width: "150px" },
+  { key: "fees", name: "Fees", width: "150px" },
+  { key: "product_cost", name: "Product Cost", width: "150px" },
+  { key: "profit", name: "Profit", width: "150px" },
 ];
 
 const orderProductsCol: Column[] = [
-  { key: "product", name: "Product", width: "100px" },
-  { key: "supplier_name", name: "Supplier Name", width: "100px" },
-  { key: "asin", name: "ASIN", width: "100px" },
-  { key: "quantity", name: "Quantity", width: "100px" },
-  { key: "amazon_sku", name: "Amazon SKU", width: "100px" },
-  { key: "current_rank", name: "Current Rank", width: "100px" },
-  { key: "30_day_rank", name: "30 Day Rank", width: "100px" },
-  { key: "90_day_rank", name: "90 Day Rank", width: "100px" },
-  { key: "units_sold", name: "Units Sold", width: "100px" },
-  { key: "velocity", name: "Velocity", width: "100px" },
+  { key: "product_name", name: "Product", width: "300px" },
+  { key: "ASIN", name: "ASIN", width: "150px" },
+  { key: "seller_sku", name: "Amazon SKU", width: "150px" },
+  { key: "quantity", name: "Quantity", width: "150px" },
+  { key: "supplier_name", name: "Supplier Name", width: "150px" },
+  { key: "thirty_days_rank", name: "30 Day Rank", width: "150px" },
+  { key: "ninety_days_rank", name: "90 Day Rank", width: "150px" },
+  { key: "units_sold", name: "Units Sold", width: "150px" },
+  { key: "product_velocity", name: "Velocity", width: "150px" },
+  { key: "lowest_fba_price", name: "Lowest FBA Price ", width: "150px" },
+  { key: "fees", name: "Fees", width: "150px" },
+  { key: "product_cost", name: "Product Cost", width: "150px" },
+  { key: "profit", name: "Profit", width: "150px" },
 ];
 
 export default function CreatePage() {
+  const { trackedProducts } = useTrackedProductContext();
+
   return (
     <IndexPageContainer>
-      <TableComponent<TrackedProducts>
+      <TableComponent<TrackedProductType>
         columns={trackedProductsCol}
-        data={products}
+        data={trackedProducts}
         actions={<InputOrderAction />}
         tableHeigth="300px"
         actionsWidth="60px"
       />
-      <TableComponent<TrackedProducts>
+      <TableComponent<TrackedProductType>
         columns={orderProductsCol}
-        data={orderProducts}
+        data={[]}
         actions={<DeleteIcon />}
         tableHeigth="300px"
         actionsWidth="60px"
