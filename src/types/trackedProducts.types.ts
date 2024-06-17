c2904019671c7c76a8b808da310b3eaad51285fb
ramirosarasola <ramiro.sarasola@gmail.com>
2024-06-17 14:59:37 -0300

    Create the TrackedProductType

---
+++ src/types/trackedProducts.types.ts
@@ -0,0 +1,14 @@
+export type TrackedProductType = {
+  product_name: string;
+  supplier_name: string;
+  asin: string;
+  amazon_sku: string;
+  '30_day_rank': number;
+  '90_day_rank': number;
+  lowest_fba_price: number;
+  fees: number;
+  product_cost: number;
+  profit: number;
+  units_sold: number;
+  velocity: number;
+}
