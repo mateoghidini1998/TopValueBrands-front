d4154ca8cb4b2769f9fc68945db3d4f02c315051
ramirosarasola <ramiro.sarasola@gmail.com>
2024-06-17 14:55:13 -0300

    fetch the trackedProducts from the context to pass it as data to the TableComponent

--- src/app/(main)/pogenerator/page.tsx
+++ src/app/(main)/pogenerator/page.tsx
@@ -1,28 +1,41 @@
-import { data } from "../../data/index";
+'use client';
+import { useTrackedProductContext } from "@/contexts/trackedProducts.context";
+// import { data } from "../../data/index";
 import IndexPageContainer from "../page.container";
 import { TableComponent } from "./components/TableComponent";
 import { Column } from "./interfaces/ITableComponent";
-import { TrackedProducts } from "./interfaces/ITrackedProducts";
+import { TrackedProductType } from "@/types/trackedProducts.types";
 
 const columns: Column[] = [
-  { key: "product", name: "Product", width: "150px" },
+  { key: "product_name", name: "Product", width: "300px" },
+  { key: "ASIN", name: "ASIN", width: "150px" },
+  { key: "seller_sku", name: "Amazon SKU", width: "150px" },
   { key: "supplier_name", name: "Supplier Name", width: "150px" },
-  { key: "asin", name: "ASIN", width: "150px" },
-  { key: "amazon_sku", name: "Amazon SKU", width: "150px" },
-  { key: "current_rank", name: "Current Rank", width: "150px" },
-  { key: "30_day_rank", name: "30 Day Rank", width: "150px" },
-  { key: "90_day_rank", name: "90 Day Rank", width: "150px" },
+  // { key: "current_rank", name: "Current Rank", width: "150px" },
+  { key: "thirty_days_rank", name: "30 Day Rank", width: "150px" },
+  { key: "ninety_days_rank", name: "90 Day Rank", width: "150px" },
   { key: "units_sold", name: "Units Sold", width: "150px" },
-  { key: "velocity", name: "Velocity", width: "150px" },
+  { key: "product_velocity", name: "Velocity", width: "150px" },
+  { key: "lowest_fba_price", name: "Lowest FBA Price ", width: "150px" },
+  { key: "fees", name: "Fees", width: "150px" },
+  { key: "product_cost", name: "Product Cost", width: "150px" },
+  { key: "profit", name: "Profit", width: "150px" },
 ];
 
 /*Here we should call all the tracked products*/
-const products: TrackedProducts[] = data.products;
+// const products: TrackedProducts[] = data.products;
+
+
 
 export default function POGeneratorPage() {
+
+  const { trackedProducts } = useTrackedProductContext();
+
+  // console.log(trackedProducts);
+
   return (
     <IndexPageContainer>
-      <TableComponent<TrackedProducts> columns={columns} data={products} />
+      <TableComponent<TrackedProductType> columns={columns} data={trackedProducts} />
     </IndexPageContainer>
   );
 }
