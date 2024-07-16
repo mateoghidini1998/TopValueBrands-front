3a17736e6b5060e3cb9b8715795ae3661787304d
mateoghidini1998 <mateghidini123@gmail.com>
2024-07-15 22:13:02 -0300

    Modify kv url

--- src/utils/kvClient.ts
+++ src/utils/kvClient.ts
@@ -3,14 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env.development.local' });
 
-const kvUrl = process.env.KV_REST_API_URL;
+const kvUrl = process.env.KV_URL;
 const kvToken = process.env.KV_REST_API_TOKEN;
 
 if (!kvUrl || !kvToken) {
   throw new Error("Missing Vercel KV configuration");
 }
 
-console.log('KV_URL:', kvUrl);
+console.log('KV_REST_API_URL:', kvUrl);
 console.log('KV_REST_API_TOKEN:', kvToken);
 
 const kv = createClient({
