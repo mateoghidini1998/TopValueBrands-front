1ab73d4a580a922106e67ad10c322aeed26206ca
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-24 12:07:53 -0300

    update dev endpoints

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -6,13 +6,13 @@ export class AuthService {
     onUserLoaded: (user: any) => void
   ) {
     const response = await HttpAPI.post(
-      "https://topvaluebrands-webapp-bjavghfxdpcgdnay.eastus-01.azurewebsites.net/api/v1/auth/login",
+      "http://localhost:5000/api/v1/auth/login",
       { email, password }
     );
     localStorage.setItem("access-token", response.token);
 
     const userResponse = await HttpAPI.get(
-      "https://topvaluebrands-webapp-bjavghfxdpcgdnay.eastus-01.azurewebsites.net/api/v1/auth/me",
+      "http://localhost:5000/api/v1/auth/me",
       {
         headers: {
           Authorization: `Bearer ${response.token}`,
@@ -27,7 +27,7 @@ export class AuthService {
   static async getUserProfile(token: string) {
     try {
       const response = await HttpAPI.get(
-        "https://topvaluebrands-webapp-bjavghfxdpcgdnay.eastus-01.azurewebsites.net/api/v1/auth/me",
+        "http://localhost:5000/api/v1/auth/me",
         {
           headers: {
             Authorization: `Bearer ${token}`,
