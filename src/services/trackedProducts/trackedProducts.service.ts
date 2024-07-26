b60c2849c4edaf9093b092bf336469f6ef98f63b
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-26 18:53:55 -0300

    update endpoint for production

--- src/services/trackedProducts/trackedProducts.service.ts
+++ src/services/trackedProducts/trackedProducts.service.ts
@@ -27,7 +27,7 @@ export class TrackedProductsService {
       params.append("orderWay", orderWay);
 
       const response = await HttpAPI.get(
-        `http://localhost:5000/api/v1/trackedproducts?${params.toString()}`,
+        `https://topvaluebrands-webapp-bjavghfxdpcgdnay.eastus-01.azurewebsites.net/api/v1/trackedproducts?${params.toString()}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
