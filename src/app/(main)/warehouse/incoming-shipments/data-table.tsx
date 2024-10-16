bdb8acba5664f4a99dadd65c35ec35478476f57b
ramiro <ramiro.sarasola@gmail.com>
2024-10-16 15:25:43 -0300

    add w-full to data-table

--- src/app/(main)/warehouse/incoming-shipments/data-table.tsx
+++ src/app/(main)/warehouse/incoming-shipments/data-table.tsx
@@ -71,7 +71,7 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div>
+    <div className="w-full">
       <div className="flex items-center pb-4">
         <Input
           placeholder="Filter by supplier..."
