3866e49179d741daebf2b97cf3e7907b9147d05c
mateoghidini1998 <mateghidini123@gmail.com>
2024-11-04 18:52:51 -0300

    Fix storage provider import

--- src/app/(main)/suppliers/page.tsx
+++ src/app/(main)/suppliers/page.tsx
@@ -1,5 +1,5 @@
 import SuppliersTable from "@/components/suppliers/SuppliersTable";
-import { StorageProvider } from "@/contexts/suppliers.context";
+import { StorageProvider } from "@/contexts/storage.context";
 
 export default function SuppliersPage() {
   return (
