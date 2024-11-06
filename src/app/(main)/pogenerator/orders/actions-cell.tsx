4b44ac12e2c6892d649d947ea79cfbfdbb9a0439
ramiro <ramiro.sarasola@gmail.com>
2024-11-06 17:26:04 -0300

    render the order summary only when clicking the view details dropdown item

--- src/app/(main)/pogenerator/orders/actions-cell.tsx
+++ src/app/(main)/pogenerator/orders/actions-cell.tsx
@@ -15,6 +15,7 @@ import { useOrdersContext } from "@/contexts/orders.context";
 import { MoreHorizontal } from "lucide-react";
 import OrderSummary from "./order-summary";
 import CustomAlertDialog from "../../../../components/ui/custom-alert-dialog";
+import { useState } from "react";
 
 type ActionsCellProps = {
   row: any;
@@ -22,6 +23,8 @@ type ActionsCellProps = {
 
 const ActionsCell = ({ row }: ActionsCellProps) => {
   const { downloadOrder, deleteOrder } = useOrdersContext();
+
+  const [isOrderSummaryOpen, setIsOrderSummaryOpen] = useState(false);
 
   const incomingOrder = row.original;
   return (
@@ -37,7 +40,10 @@ const ActionsCell = ({ row }: ActionsCellProps) => {
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DialogTrigger asChild>
-              <DropdownMenuItem className="w-full">
+              <DropdownMenuItem
+                className="w-full"
+                onClick={() => setIsOrderSummaryOpen(true)}
+              >
                 View Details
               </DropdownMenuItem>
             </DialogTrigger>
@@ -56,7 +62,7 @@ const ActionsCell = ({ row }: ActionsCellProps) => {
         </DropdownMenu>
 
         {/* Dialog for View Details */}
-        <OrderSummary orderId={incomingOrder.id} />
+        {isOrderSummaryOpen && <OrderSummary orderId={incomingOrder.id} />}
       </Dialog>
     </div>
   );
