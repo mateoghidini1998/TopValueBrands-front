9697f93e1065ae88de2a48fac2456d609d59f5c7
ramiro <ramiro.sarasola@gmail.com>
2024-12-09 11:30:09 -0300

    add back button for every page

--- src/components/layout/PageTitlte.tsx
+++ src/components/layout/PageTitlte.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { MAIN_ROUTES } from "@/constants/links";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { BiArrowBack } from "react-icons/bi";
 
 export default function PageTitle() {
   const pathname = usePathname();
+  const router = useRouter();
   const currentMainRoute = MAIN_ROUTES.find((link) => link.href === pathname);
   return (
     <>
       <div className="flex items-center gap-2">
+        <BiArrowBack
+          className="text-black dark:text-white cursor-pointer"
+          onClick={() => router.back()}
+        />
         {currentMainRoute?.icon}
         <h4 className="text-black dark:text-white text-base font-bold leading-6">
           {currentMainRoute?.title}
