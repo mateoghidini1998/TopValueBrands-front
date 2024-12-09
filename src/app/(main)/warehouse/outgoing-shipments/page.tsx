f5f698c47ef6e760e9a92eed4871f4b9ffadc85d
ramiro <ramiro.sarasola@gmail.com>
2024-12-09 12:19:20 -0300

    transform outgoing shipments into a client componente page

--- src/app/(main)/warehouse/outgoing-shipments/page.tsx
+++ src/app/(main)/warehouse/outgoing-shipments/page.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { DataTable } from "@/components/ui/data-table";
 import { ShipmentsService } from "@/services/shipments/shipments.service";
 import IndexPageContainer from "../../page.container";
 import { columns } from "./columns";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default async function OutgoingShipments() {
-  const shipments = await ShipmentsService.getShipments();
+export default function OutgoingShipments() {
+  const [shipments, setShipments] = useState([]);
+
+  useEffect(() => {
+    const fetchShipments = async () => {
+      const response = await ShipmentsService.getShipments();
+      setShipments(response.shipments);
+    };
+    fetchShipments();
+  }, []);
+
   return (
     <IndexPageContainer>
       <div className="w-full px-[1.3rem] py-0">
@@ -15,7 +27,7 @@ export default async function OutgoingShipments() {
             New Shipment
           </Link>
         </Button>
-        <DataTable columns={columns} data={shipments.shipments} />
+        <DataTable columns={columns} data={shipments} />
       </div>
     </IndexPageContainer>
   );
